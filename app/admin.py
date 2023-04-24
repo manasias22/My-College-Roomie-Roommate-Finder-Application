d9b10270a2fac66743637e12d6f45cd48a3e9c0f
MRUDUL PARAG CHAUDHARI <103252162+manasias22@users.noreply.github.com>
2023-04-25 01:19:42 +0530

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . import models
+
+
+# Register your models here.
+admin.site.register(models.Contact)
+admin.site.register(models.Vacantroom)
+admin.site.register(models.Register)
+admin.site.register(models.Roommate)
+admin.site.register(models.Sellroom)
+admin.site.register(models.Vacanthouse)
+
+
